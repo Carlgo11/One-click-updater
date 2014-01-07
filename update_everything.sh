dd148a986085235dab55ddf0452fda6f553c8f9e
Carlgo11 <Carlgo11@portalcraft.se>
2014-01-07 01:12:00 +0100

    Renamed update_servers.sh to update_everything.sh

--- update_everything.sh
+++ update_everything.sh
@@ -1,10 +1,10 @@
-cd /home/hubserver/bungee/
+cd '/home/hubserver/bungee/' #Replace with your own bungeecord path
 rm BungeeCord.jar
 wget http://ci.md-5.net/job/BungeeCord/lastSuccessfulBuild/artifact/bootstrap/target/BungeeCord.jar
 chmod 700 BungeeCord.jar
 echo "BungeeCord updated!"
 
-cd /home/hubserver/HUB_SERVER/
+cd '/home/hubserver/HUB_SERVER/' #Replace with your own server path
 rm ./spigot.jar
 wget http://ci.md-5.net/view/Spigot/job/Spigot/lastSuccessfulBuild/artifact/Spigot-Server/target/spigot.jar
 chmod 700 spigot.jar
